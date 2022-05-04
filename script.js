7e98b344b3bc53930e4f66770bcfa7bfbace97c1
Rowings <zakariaazarkan@gmail.com>
2022-05-04 10:10:53 +0200

    envoie des premier fichier

---
+++ script.js
@@ -0,0 +1,10 @@
+// let tabJeu = [
+//     [0,0,0,0]
+//     [0,0,0,0]
+//     [0,0,0,0]
+//     [0,0,0,0]
+// ];
+
+
+const divResultat = document.querySelector("#resultat");
+divResultat.innerHtml = "Coucou"
